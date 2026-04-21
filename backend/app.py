d4a38c1ec02e4b2cf57568394748c36ebf0a7dcb
Ariel Liotto Angonese <ariel.angonese12345@gmail.com>
2026-04-21 01:11:10 -0300

    feat: adiciona rota POST /deliveries

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from database import get_all_deliveries, get_delivery_by_id
+from database import get_all_deliveries, get_delivery_by_id, insert_delivery, insert_address
 
 # Criação da aplicação Flask
 app = Flask(__name__)
@@ -19,3 +19,35 @@ def get_delivery(id):
     if delivery is None:
         return jsonify({"erro": "Entrega não encontrada"}), 404
     return jsonify(delivery)
+
+# Rota para criar uma nova entrega
+@app.route("/deliveries", methods=["POST"])
+def create_delivery():
+    data = request.get_json()
+
+    id_origin_address = insert_address(
+        data["origin_street"],
+        data["origin_number"],
+        data["origin_city"],
+        data["origin_lat"],
+        data["origin_lng"]
+    )
+
+    id_destination_address = insert_address(
+        data["destination_street"],
+        data["destination_number"],
+        data["destination_city"],
+        data["destination_lat"],
+        data["destination_lng"]
+    )
+
+    id_delivery = insert_delivery(
+        status="pendente",
+        data=data["date"],
+        id_usuario=data["user_id"],
+        id_destinatario=data["recipient_id"],
+        id_endereco_origem=id_origin_address,
+        id_endereco_destino=id_destination_address
+    )
+
+    return jsonify({"delivery_id": id_delivery}), 201
