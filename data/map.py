5e2fd6d667e97a178fba1669a3669c6432d62ec4
Ariel Liotto Angonese <ariel.angonese12345@gmail.com>
2026-04-20 21:54:48 -0300

    feat: adiciona função de carregamento do mapa da cidade

--- data/map.py
+++ data/map.py
@@ -1,5 +1,11 @@
 import osmnx as ox
 
+# Função de geocodificação para converter um endereço em coordenadas geográficas
 def geocode_address(address):
     location = ox.geocode(address)
     return location
+
+# Função para carregar o mapa da cidade usando OSMnx
+def load_map(city):
+    graph = ox.graph_from_place(city, network_type="drive")
+    return graph
